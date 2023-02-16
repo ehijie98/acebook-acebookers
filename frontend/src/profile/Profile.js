f2086187307297b6be159e56139b572478412461
Zahar <zahar.zelenskiy@gmail.com>
2023-02-16 17:01:42 +0000

    add user profile attributes

--- frontend/src/profile/Profile.js
+++ frontend/src/profile/Profile.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./Profile.css";
 
-const Profile = ({userId}) => {
+const Profile = ({ userId }) => {
   const [user, setUser] = useState(null);
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch(`/users/user${userId}`)
-      .then(response => response.json())
-      .then(data => setUser(data))
-      .catch(error => console.log(error));
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.log(error));
   }, [userId]);
 
-  if (!user){
+  if (!user) {
     return (
       <button class="btn btn-primary" type="button" disabled>
         <span
@@ -22,26 +22,28 @@ const Profile = ({userId}) => {
         Loading...
       </button>
     );
-  }
-  return (
-    <div class="card text-center">
-      <div class="card-header"></div>
-      <div class="card-body">
-        <h5 class="card-title">{user.firstName}{user.lastName}</h5>
-        <p class="card-text">
-          date of birth
-          post count
-          user since
-        </p>
-        <a href="/posts/:id" class="btn btn-primary">
-          See Posts
-        </a>
+  } else {
+    return (
+      <div class="card text-center">
+        <div class="card-header"></div>
+        <div class="card-body">
+          <h5 class="card-title">
+            {user.firstName}
+            {user.lastName}
+            {user.dateOfBirth}
+            {user.dateJoined}
+            {user.allPosts}
+            {user.postsCount}
+          </h5>
+          <p class="card-text">date of birth post count user since</p>
+          <a href="/posts/:id" class="btn btn-primary">
+            See Posts
+          </a>
+        </div>
+        <div class="card-footer text-muted"> last login 2 days ago</div>
       </div>
-      <div class="card-footer text-muted"> last login 2 days ago</div>
-    </div>
-  );
-
-
-}
+    );
+  }
+};
 
 export default Profile;
