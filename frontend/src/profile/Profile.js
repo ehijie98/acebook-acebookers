52dfe42941118756e8a596bf1f5d2736bc4ac584
Zahar <zahar.zelenskiy@gmail.com>
2023-02-17 03:07:52 +0000

    update profile page

--- frontend/src/profile/Profile.js
+++ frontend/src/profile/Profile.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Profile.css";
+// import "./SignUpForm.js";
 
 const Profile = ({ userId }) => {
   const [user, setUser] = useState(null);
